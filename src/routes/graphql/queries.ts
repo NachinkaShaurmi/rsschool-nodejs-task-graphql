0b82b759dffbeca6e5c21fe99496ab5cb773fbee
iurii-oleinik <iurii.oleinik@alpeso.io>
2025-05-25 13:33:19 +0300

    feat: add loader-prime

--- src/routes/graphql/queries.ts
+++ src/routes/graphql/queries.ts
@@ -5,6 +5,13 @@ import { UserType } from './types/user.js';
 import { UUIDType } from './types/uuid.js';
 import { PostType } from './types/post.js';
 import { ProfileType } from './types/profile.js';
+import {
+  parseResolveInfo,
+  ResolveTree,
+  simplifyParsedResolveInfoFragmentWithType,
+} from 'graphql-parse-resolve-info';
+
+type UserFields = 'profile' | 'posts' | 'userSubscribedTo' | 'subscribedToUser';
 
 export const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -26,7 +33,48 @@ export const RootQueryType = new GraphQLObjectType({
 
     users: {
       type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(UserType))),
-      resolve: (_source, _args, context: GQLContext) => context.prisma.user.findMany(),
+      resolve: async (_source, _args, context, info) => {
+        const parsed = parseResolveInfo(info);
+        const fields = Object.keys(
+          simplifyParsedResolveInfoFragmentWithType(
+            parsed as ResolveTree,
+            new GraphQLList(UserType),
+          ).fields,
+        );
+
+        const include: Partial<Record<UserFields, boolean>> = {};
+
+        if (fields.includes('profile')) include.profile = true;
+        if (fields.includes('posts')) include.posts = true;
+        if (fields.includes('userSubscribedTo')) include.userSubscribedTo = true;
+        if (fields.includes('subscribedToUser')) include.subscribedToUser = true;
+
+        const users = await context.prisma.user.findMany({ include });
+
+        if (fields.includes('userSubscribedTo')) {
+          for (const user of users) {
+            const related = users.filter(
+              (u) =>
+                Array.isArray(u.subscribedToUser) &&
+                u.subscribedToUser.some((rel) => rel.subscriberId === user.id),
+            );
+            context.loaders.userSubscribedToLoader.clear(user.id).prime(user.id, related);
+          }
+        }
+
+        if (fields.includes('subscribedToUser')) {
+          for (const user of users) {
+            const related = users.filter(
+              (u) =>
+                Array.isArray(u.userSubscribedTo) &&
+                u.userSubscribedTo.some((rel) => rel.authorId === user.id),
+            );
+            context.loaders.subscribedToUserLoader.clear(user.id).prime(user.id, related);
+          }
+        }
+
+        return users;
+      },
     },
 
     user: {
